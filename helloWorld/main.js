906bdf1abd65cd68e71c7234646f123913e9ae78
avasquet <alejandro.vasquez.taborda@gmail.com>
2022-06-11 20:50:02 -0500

    Prints 'Hello World\!' to console

--- helloWorld/main.js
+++ helloWorld/main.js
@@ -11,3 +11,9 @@ class Solution{
   console.log('Hello World!');
     }
 }
+
+//refer to the example and write your first JS function
+function helloWorld (){
+  let str = 'Hello World!';
+  console.log(str);
+}
