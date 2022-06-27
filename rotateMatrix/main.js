71ae91ea0165b0495e4691ec404bd4c54518caee
avasquet <alejandro.vasquez.taborda@gmail.com>
2022-06-27 14:47:56 -0500

    Rotates a matrix 90 degrees to the left

--- rotateMatrix/main.js
+++ rotateMatrix/main.js
@@ -1,5 +1,10 @@
-function countSheeps(arrayOfSheep) {
-    // TODO May the force be with you
-    return arrayOfSheep.reduce((acum, sheep) => (sheep === true) ? acum + 1 : acum + 0)
+var rotateMatrix = arr => {
+  let rotated = [];
+  for (let i = arr[0].length - 1; i >= 0; i--){
+    rotated[arr[0].length - 1  - i] = [];
+    for(let j = 0; j < arr.length; j++){
+      rotated[arr[0].length - 1 - i].push(arr[j][i]);
+    }
   }
-
+  return rotated;
+}
