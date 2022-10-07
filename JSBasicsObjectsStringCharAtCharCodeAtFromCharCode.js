64cff9ea942dc660b5a0fba9939904985209e6f7
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-07 12:06:16 -0500

    Basic operations with Objects, Strings, fromCharCode, charCodeAt and charAt

---
+++ JSBasicsObjectsStringCharAtCharCodeAtFromCharCode.js
@@ -0,0 +1,13 @@
+function topSecret(str){
+    return str.split(' ').map(
+        str => str.split('').map(i =>
+            i.replace(/[a-z]/i, char => String.fromCharCode(i.charCodeAt() + ((i.charCodeAt() < 100 && i.charCodeAt() > 96) ? 23 : (i.charCodeAt() < 68) ? 23 : -3)))
+        )
+        .join('')
+    ).join(' ');
+}
+
+console.log(topSecret('abcABC'));
+console.log(topSecret('Pb qdph lv Mrkq'));
+console.log(topSecret('wklv lv dq hadpsoh'));
+console.log(topSecret('Khoor Zruog!'));
