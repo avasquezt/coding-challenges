e9b84b7576f0355f728e3feaa3e8a4d0f008de18
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-11 12:12:45 -0500

    Basic operations with Regular Expressions, Lookahead and Lookbehind

--- JSBasicsRegExpLookaheadLookbehind.js
+++ JSBasicsRegExpLookaheadLookbehind.js
@@ -3,3 +3,9 @@ function addCommas(money,reg){
   //coding the use of regex here...
   return money.split('').reverse().join('').replace(reg,x=>x+",").split('').reverse().join('');  //like this
 }
+
+// var regex=/\d(?=(\d{3})+$)/g
+// function addCommas(money,reg){
+//   //coding the use of regex here...
+//   return money.replace(reg,x=>x+",")  //like this
+// }
