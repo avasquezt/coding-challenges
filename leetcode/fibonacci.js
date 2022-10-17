94eb24a4c9da707a92f049382df92d0494116e16
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-10-17 16:28:24 -0500

    Calculates the Fibonacci term requested

---
+++ leetcode/fibonacci.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ var fib = function(n) {
+    let fibSequence = [];
+    fibSequence[0] = 0;
+    fibSequence[1] = 1;
+    for(let i = 2; i <= n; i++){
+        fibSequence[i] =  fibSequence[i - 1] +  fibSequence[i - 2];
+    }
+    return fibSequence[n];
+};
