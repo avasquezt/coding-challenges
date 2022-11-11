9a0621c38c74decb1017b769c6563f21b344a9e8
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-11-11 11:24:22 -0500

    Removes the duplicates of the given array in place

---
+++ leetcode/removeDuplicatesInPlace.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+    let n = 0;
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[n]){
+            n++;
+            nums[n] = nums[i];
+        }
+    }
+    return n + 1;
+};
