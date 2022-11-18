503406e508d7e712cfc9e4dd1fc4b6053c3b763e
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-11-18 13:31:21 -0500

    Creates a hashtag with the given sentence

--- codewars/generateHashtag.js
+++ codewars/generateHashtag.js
@@ -4,3 +4,12 @@ function generateHashtag (str) {
     str = str.split(/\s+/).map(word => word[0].toUpperCase() + word.slice(1)).join('');
     return str.length < 140 ? '#' + str : false;
   }
+
+function generateHashtag(str){
+  str = '# '.concat(str).trim().split(/\s+/).map(word => word[0].toUpperCase() + word.slice(1)).join('');
+  return str.length > 1 && str.length < 140 ? str : false;
+}
+
+console.log(generateHashtag('   '));
+console.log(generateHashtag('a b c d'));
+console.log(generateHashtag(' a b c d '));
