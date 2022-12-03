75572f13ca7473321a2d53288b9e087bbb40b692
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-12-03 10:01:16 -0500

    Returns the input string characters in reversed order

--- codewars/reverseString.js
+++ codewars/reverseString.js
@@ -1,3 +1,13 @@
 function reverse(string){
     return string.split(' ').reverse().join(' ');
   }
+
+function reverse(string){
+  let result = '';
+  for(const char of string){
+    result = char + result;
+  }
+  return result;
+}
+
+console.log(reverse('abcde'))
