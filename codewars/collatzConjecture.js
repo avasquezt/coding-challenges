8bb9310ee7e77b3b7b81717640457743f993c0df
avasquezt <alejandro.vasquez.taborda@gmail.com>
2022-12-18 11:59:13 -0500

    Calculates the number of times needed to reduce the input number to one, by applying the Collatz conjecture

---
+++ codewars/collatzConjecture.js
@@ -0,0 +1,12 @@
+var hotpo = function(n){
+    let count = 0;
+    while(n > 1){
+      if(n % 2){
+        n = 3 * n + 1;
+      }else{
+        n /= 2;
+      }
+      count++;
+    }
+    return count;
+}
