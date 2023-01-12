0adcc895e05daad76f198ea8d51f863e4e24b0e1
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-01-12 11:49:43 -0500

    Sorts the characters in the input string

---
+++ codewars/sortString.js
@@ -0,0 +1,9 @@
+/**
+ * Sorts the characters in the input string
+ * @param {string} code 
+ * @returns {string}
+ */
+
+function sortGiftCode(code){
+    return code.split('').sort().join('');
+  }
