977b500a89b7f27f253b412b5c5529d0e2997ec2
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-01-21 12:13:50 -0500

    Calculates the result of x to the power of n

---
+++ codewars/power.js
@@ -0,0 +1,31 @@
+/**
+ * Calculates the result of x to the power of n
+ * @param {number} x 
+ * @param {number} n 
+ * @returns {number}
+ */
+
+function numberToPower(x, n){
+  
+    if (n < 0){
+        x =  1 / x;
+        n = -n;
+    }
+
+    if (n == 0) return 1;
+
+    let y = 1;
+    while (n > 1){
+        if (n % 2 == 0){
+        x *= x;
+        n /= 2;
+        }else{
+        y *= x;
+        x *= x;
+        n = (n - 1) / 2;
+        }
+    }
+
+    return x * y;
+
+}
