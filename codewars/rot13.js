e7e9dd9818e4bac30af2c705d933282d690739d7
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-02-02 15:57:09 -0500

    Encodes the input string using the ROT13 cipher

--- codewars/rot13.js
+++ codewars/rot13.js
@@ -1,4 +1,15 @@
+/**
+ * Encodes the input string using the ROT13 cipher
+ * @param {string} message 
+ * @returns {string}
+ */
 function rot13(message){
-    let arr = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
-    return message.replace(/[a-z]/gi, char => arr[arr.indexOf(char) + 13]);
-  }
+  let arr = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLM';
+  return message.replace(/[a-z]/gi, char => arr[arr.indexOf(char) + 13]);
+}
+
+// function rot13(str) {
+//   return str.replace(/[a-z]/ig, function(x){
+//     return String.fromCharCode(x.charCodeAt(0) + (x.toLowerCase() <= 'm' ? 13: -13));
+//   });
+// }
