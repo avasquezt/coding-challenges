3036bd3424a58e8da1b257d344f3ad98ac6ac312
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-02-03 14:32:55 -0500

    Hero class definition

---
+++ codewars/heroClass.js
@@ -0,0 +1,12 @@
+/**
+ * Hero class definition
+ * @param {string} name 
+ */
+
+function Hero (name) {
+    this.name = name || 'Hero';
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+}
