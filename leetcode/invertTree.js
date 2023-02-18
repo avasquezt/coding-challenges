32d0bd5e0e229dbe3e79419068fe41fc522d913b
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-02-18 18:37:29 -0500

    Inverts the nodes of the input tree

---
+++ leetcode/invertTree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Inverts the nodes of the input tree
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if(root){
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
+        [root.left, root.right] = [root.right, root.left];
+    }
+    return root;
+};
