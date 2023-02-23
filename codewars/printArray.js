050a836ea0235faa4a71dffaf6ff9f61f049e21b
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-02-23 14:03:42 -0500

    Prints the elements from the input array delimited by commas

---
+++ codewars/printArray.js
@@ -0,0 +1,9 @@
+/**
+ * Prints the elements from the input array delimited by commas 
+ * @param {array} array 
+ * @returns {string}
+ */
+
+function printArray(array){
+    return array.join();
+}
