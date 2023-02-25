83452e331eb9b611969029279482a47138b18273
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-02-25 14:36:00 -0500

    Checks if the input parameters have the same value

---
+++ codewars/coercion.js
@@ -0,0 +1,10 @@
+/**
+ * Checks if the input parameters have the same value
+ * @param {string|number} a 
+ * @param {string|number} b 
+ * @returns {boolean}
+ */
+
+function add(a, b){
+	return a == b;
+}
