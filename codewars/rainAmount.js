b946ea70585cd39e8996cccb9a0cb298aa2c230e
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-03-25 13:41:18 -0500

    Calculates the ammount of water the plants need

---
+++ codewars/rainAmount.js
@@ -0,0 +1,13 @@
+/**
+ * Calculates the ammount of water the plants need 
+ * @param {number} mm 
+ * @returns {string}
+ */
+function rainAmount(mm){
+    if (mm < 40) {
+         return "You need to give your plant " + (40 - mm) + "mm of water";
+    }
+    else {
+         return "Your plant has had more than enough water for today!"
+    };
+}
