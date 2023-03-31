a9931670e63e063cfc4a3244e32cee38e9b937ae
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-03-31 13:10:12 -0500

    Flattens and sorts the input array

---
+++ codewars/flattenAndSort.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * Flattens and sorts the input array
+ * @param {number[][]} array 
+ * @returns {number[]}
+ */
+function flattenAndSort(array) {
+  // Good luck, brave code warrior!
+  return array.flat().sort((a, b) => a - b);
+}
