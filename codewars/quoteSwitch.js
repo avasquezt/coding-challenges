bbf8a8ca88f39f79d60a6e1e03c6ceafbb8c3102
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-04-20 13:34:08 -0500

    Returns a different quote, depending on the input name

---
+++ codewars/quoteSwitch.js
@@ -0,0 +1,8 @@
+/**
+ * Returns a different quote, depending on the input name
+ * @param {string} fighter 
+ * @returns {string}
+ */
+var quote = function(fighter) {
+    return fighter.toLowerCase() == 'george saint pierre' ? 'I am not impressed by your performance.' : "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
