241a2108a4c97e7233f77bc2429eaa17e12f0e37
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-05-17 18:16:58 -0500

    Swaps every two adjacent nodes from the input list

---
+++ leetcode/swapPairsLinkedList.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * Swaps every two adjacent nodes from the input list
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    let currNode = head;
+    let prevNode;
+    while(currNode){
+        if(prevNode){
+            [prevNode.val, currNode.val] = [currNode.val, prevNode.val];
+            prevNode = null;
+        }else{
+            prevNode = currNode;
+        }
+        currNode = currNode.next;
+    }
+    return head;
+};
