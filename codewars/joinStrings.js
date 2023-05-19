6a88c44f8cc2a968bcabfb151dda1d44fbbb875d
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-05-19 16:55:19 -0500

    Merges the input strings

---
+++ codewars/joinStrings.js
@@ -0,0 +1,9 @@
+/**
+ * Merges the input strings
+ * @param {string} string1 
+ * @param {string} string2 
+ * @returns {string}
+ */
+function joinStrings(string1, string2){
+   return `${string1} ${string2}`;
+}
