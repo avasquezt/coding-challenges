6bc25d57a67d0c154e0190eecde2aa42b80cade8
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-05-29 17:39:13 -0500

    Replaces all occurrences of the string 'Fire' with '~~' in the input string

---
+++ codewars/replaceAllOcurrencesString.js
@@ -0,0 +1,8 @@
+/**
+ * Replaces all occurrences of the string 'Fire' with '~~' in the input string
+ * @param {string} s 
+ * @returns {string}
+ */
+function fireFight(s){
+  return s.replace(/Fire/g, '~~');
+}
