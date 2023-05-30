845692aeae3b130d66f5aee64c028b780fad6d9a
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-05-30 17:36:10 -0500

    Parking system class for managing parking spaces in a parking lot

---
+++ leetcode/parkingSystem.js
@@ -0,0 +1,23 @@
+/**
+ * Parking system class for managing parking spaces in a parking lot
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.carType = [0, big, medium, small]; 
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    return --this.carType[carType] >= 0;
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
