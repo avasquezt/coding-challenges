0bb8d8fe291597fa75b00f8651bec1a63e9d5cf9
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-06-15 17:25:54 -0500

    Truncates the input number to two decimal numbers

--- codewars/twoDecimalPlaces.js
+++ codewars/twoDecimalPlaces.js
@@ -1,3 +1,17 @@
+/**
+ * Rounds up the input number to two decimal numbers
+ * @param {number} n 
+ * @returns {number}
+ */
 function twoDecimalPlaces(n) {
-    return Number(n.toFixed(2));
-  }
+  return Number(n.toFixed(2));
+}
+
+/**
+ * Truncates the input number to two decimal numbers
+ * @param {number} number 
+ * @returns {number}
+ */
+function twoDecimalPlaces(number) {
+  return Math.trunc(number * 100)/100;
+}
