eb8853ae7d7a78907b0f1be554f7f255f5718163
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-07-21 13:22:38 -0500

    Returns a greeting for the input name or null if it is falsy

---
+++ codewars/greetOrNull.js
@@ -0,0 +1,6 @@
+/**
+ * Returns a greeting for the input name or null if it is falsy
+ * @param {string} name 
+ * @returns {string | null}
+ */
+const greet = name => name ? `hello ${name}!` : null;
