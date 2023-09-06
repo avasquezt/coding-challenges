31591efd4989f882f85e8359fb628f788d71cbcd
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-09-06 15:20:26 -0500

    Removes the requested value from the input array in place

---
+++ leetcode/removeElementArray.js
@@ -0,0 +1,19 @@
+/**
+ * Removes the requested value from the input array in place
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let currIndex = 0;
+    let next = nums.length - 1;
+    while(currIndex <= next){
+        if(nums[currIndex] == val){
+            while(nums[next] == val && next > currIndex) next--;
+            [nums[currIndex], nums[next]] = [nums[next], nums[currIndex]];
+            next--;
+        }
+        currIndex++;
+    }
+    return next + 1;
+};
