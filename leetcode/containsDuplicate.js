be0a12067a3022542da19b0de61b856cb2b9d51c
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-09-24 16:34:30 -0500

    Checks if any value in the input array appears at least twice

--- leetcode/containsDuplicate.js
+++ leetcode/containsDuplicate.js
@@ -1,14 +1,15 @@
 /**
+ * Checks if any value in the input array appears at least twice
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const seen = new Map();
+    const seen = new Set();
     for(const n of nums){
         if(seen.has(n)){
             return true;
         }
-        seen.set(n, true);
+        seen.add(n);
     }
     return false;
 };
