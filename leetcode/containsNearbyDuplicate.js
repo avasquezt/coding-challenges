9e31049a0ddf8cbf8e651292c033ab4d901770b1
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-09-30 17:33:22 -0500

    Checks if there are two elements in the input array with the same value within the requested distance

--- leetcode/containsNearbyDuplicate.js
+++ leetcode/containsNearbyDuplicate.js
@@ -1,4 +1,5 @@
 /**
+ * Checks if there are two elements in the input array with the same value within the requested distance
  * @param {number[]} nums
  * @param {number} k
  * @return {boolean}
@@ -10,6 +11,16 @@
             if (Math.abs(i-seen.get(nums[i])) <= k) return true;
         }
         seen.set(nums[i], i);
+    }
+    return false;
+};
+
+var containsNearbyDuplicate = function(nums, k) {
+    const window = new Set();
+    for(let i = 0; i < nums.length; i++){
+        if(i > k) window.delete(nums[i - k - 1]);
+        if(window.has(nums[i])) return true;
+        window.add(nums[i]);
     }
     return false;
 };
