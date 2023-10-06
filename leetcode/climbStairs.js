2d03ac7019e146c1378506cca60c8de7b40157e4
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-10-06 15:47:53 -0500

    Calculates the number of steps needed to climb a set of stairs with the requested size

--- leetcode/climbStairs.js
+++ leetcode/climbStairs.js
@@ -1,11 +1,28 @@
 /**
+ * Calculates the number of steps needed to climb a set of stairs with the requested size
  * @param {number} n
  * @return {number}
  */
 var climbStairs = function(n) {
     let [a, b] = [1, 2];
-    while(steps.length < n){
+    n -= 2;
+    while(n--){
         [a, b] = [b, a + b];
     }
     return b;
+};
+
+/**
+ * Recursive
+ */
+var climbStairs = function(n) {
+    const cache = new Map();
+    function helper(n){
+        if(n < 2) return 1;
+        if(cache.has(n)) return cache.get(n);
+        const result = helper(n - 1) + helper(n - 2);
+        cache.set(n, result);
+        return result;
+    }
+    return helper(n);
 };
