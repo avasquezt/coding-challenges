644b9aae6332080986eb776b57aa186a1d263d50
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-11-13 15:24:53 -0500

    Checks if it the input tree is a valid binary search tree (BST)

---
+++ leetcode/isValidBST.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Checks if it the input tree is a valid binary search tree (BST)
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root, min = -Infinity, max = Infinity) {
+    if(!root) return true;
+    return (root.val > min && root.val < max) && isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+};
