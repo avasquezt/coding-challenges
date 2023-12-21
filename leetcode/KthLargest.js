61696691a5dc772875f8a1dcaee0841475af4273
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-12-21 15:57:58 -0500

    Class to find the kth largest element in a stream

--- leetcode/KthLargest.js
+++ leetcode/KthLargest.js
@@ -24,3 +24,77 @@ KthLargest.prototype.add = function(val) {
  * var obj = new KthLargest(k, nums)
  * var param_1 = obj.add(val)
  */
+
+var KthLargest = function(k, nums) {
+    this.head = null;
+    this.k = k;
+    
+    this.addVal = function(node, val){
+        if(node){
+            node.count++;
+            if(val <= node.val){
+                node.left = this.addVal(node.left, val);
+            }else{
+                node.right = this.addVal(node.right, val);
+            }
+        }else{
+            node = new TreeNode(val, 1);
+        }
+        return node;
+    }
+    
+    for(const n of nums){
+        this.head = this.addVal(this.head, n);
+    };
+    
+};
+
+// Solution using BST
+var KthLargest = function(k, nums) {
+    this.head = null;
+    this.k = k;
+    
+    this.addVal = function(node, val){
+        if(node){
+            node.count++;
+            if(val <= node.val){
+                node.left = this.addVal(node.left, val);
+            }else{
+                node.right = this.addVal(node.right, val);
+            }
+        }else{
+            node = new TreeNode(val, 1);
+        }
+        return node;
+    }
+    
+    for(const n of nums){
+        this.head = this.addVal(this.head, n);
+    };
+}
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.head = this.addVal(this.head, val);
+    return search(this.head, this.k);
+    
+    function search(node, n){
+        const k = n - (node.right?.count || 0);
+        if(k == 1) return node.val;
+        if(k > 0){
+            return search(node.left, k - 1);
+        }else{
+            return search(node.right, n);
+        }
+    }
+};
+
+function TreeNode(val, count){
+    this.val = val;
+    this.count = count;
+    this.left = null;
+    this.right = null;
+}
