2f74307988ae6bd2de93f7a1bfb56b3e522ab33f
avasquezt <alejandro.vasquez.taborda@gmail.com>
2023-12-28 15:05:00 -0500

    Checks if there are two numbers in the input array with a difference of valueDiff and a distance of indexDiff

--- leetcode/containsNearbyAlmostDuplicate.js
+++ leetcode/containsNearbyAlmostDuplicate.js
@@ -16,3 +16,71 @@ var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
     }
     return false;
 }
+
+
+/**
+ * With BST
+ * @param {number[]} nums
+ * @param {number} indexDiff
+ * @param {number} valueDiff
+ * @return {boolean}
+ */
+var containsNearbyAlmostDuplicate = function(nums, indexDiff, valueDiff) {
+    let bst = null;
+    let isValid = false;
+    for(let i = 0; i < nums.length; i++){
+        
+        [bst, isValid] = add(bst, nums[i], valueDiff);
+        
+        if(isValid) return true;
+        
+        if(i >= indexDiff){
+            bst = remove(bst, nums[i - indexDiff]);
+        }
+        
+    }
+    return false;
+};
+
+function TreeNode(val){
+    this.val = val;
+    this.left = null;
+    this.right = null;
+}
+
+function add(node, val, valueDiff){
+    let isValid = false;
+    if(node){
+        if(val <= node.val){
+            [node.left, isValid] = add(node.left, val, valueDiff);
+        }else{
+            [node.right, isValid] = add(node.right, val, valueDiff);
+        }
+        isValid = isValid || Math.abs(node.val - val) <= valueDiff;
+    }else{
+        node = new TreeNode(val);
+    }
+    return [node, isValid];
+}
+
+function remove(node, val){
+    if(node){
+        if(node.val == val){
+            if(node.left && node.right){
+                let curr = node.right;
+                while(curr.left){
+                    curr = curr.left;
+                }
+                node.right = remove(node.right, curr.val);
+                node.val = curr.val;
+            }else{
+                return node.left || node.right;
+            }
+        }else if(val < node.val){
+            node.left = remove(node.left, val);            
+        }else{
+            node.right = remove(node.right, val)
+        }
+    }
+    return node;
+}
