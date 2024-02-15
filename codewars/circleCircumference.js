9cc7f1e83343dbadbeb5eb5aa4439c87fec26536
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-02-15 14:05:50 -0500

    Calculates the circumference of the given circle

---
+++ codewars/circleCircumference.js
@@ -0,0 +1,8 @@
+/**
+ * Calculates the circumference of the given circle
+ * @param {circle} circle 
+ * @returns {number}
+ */
+function circleCircumference(circle) {
+    return 2 * Math.PI * circle.radius;
+}
