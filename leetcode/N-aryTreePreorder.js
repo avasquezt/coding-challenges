8f3463752667b99fe1e7c67597f847de193267bd
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-03-05 14:40:10 -0500

    Returns the preorder traversal of the input n-ary tree

--- leetcode/N-aryTreePreorder.js
+++ leetcode/N-aryTreePreorder.js
@@ -6,7 +6,7 @@
  * };
  */
 
-/**
+/** Recursive
  * Returns the preorder traversal of the input n-ary tree
  * @param {Node|null} root
  * @return {number[]}
@@ -16,4 +16,22 @@ var preorder = function(root) {
     let nodes = [root.val];
     root.children.forEach(n => nodes = nodes.concat(preorder(n)));
     return nodes;
+};
+
+
+/**
+ * Iterative
+ */
+var preorder = function(root) {
+    if(!root) return [];
+    const stack = [root];
+    const ans = [];
+    while(stack.length){
+        const curr = stack.pop();
+        ans.push(curr.val);
+        for(let i = curr.children.length - 1; i >= 0; i--){
+            stack.push(curr.children[i]);
+        }
+    }
+    return ans;
 };
