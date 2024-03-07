669ae603c3d9e8739580b039e437e306e4da5e83
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-03-07 14:57:03 -0500

    Returns the postorder traversal of the input n-ary tree

--- leetcode/N-aryTreePostorder.js
+++ leetcode/N-aryTreePostorder.js
@@ -6,7 +6,7 @@
  * };
  */
 
-/**
+/** Recursive
  * Returns the postorder traversal of the input n-ary tree
  * @param {Node|null} root
  * @return {number[]}
@@ -18,4 +18,21 @@ var postorder = function(root, ans = []) {
     }
     ans.push(root.val);
     return ans;
+};
+
+/**
+ * Iterative
+ */
+var postorder = function(root) {
+    if (!root) return [];
+    const stack = [root];
+    const ans = [];
+    while(stack.length){
+        const curr = stack.pop();
+        ans.push(curr.val);
+        for(const child of curr.children){
+            stack.push(child);
+        }
+    }
+    return ans.reverse();
 };
