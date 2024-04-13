ba683695e12c2a50669add62807019f13aac11c1
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-04-13 13:58:11 -0500

    Capitalizes the characters from the input string in the even and odd indexes

--- codewars/capitalizeString.js
+++ codewars/capitalizeString.js
@@ -7,4 +7,14 @@ String.prototype.capitalize = function(){
     let firstCharCode = this.charCodeAt(0);
     firstCharCode = firstCharCode > 96 && firstCharCode < 123 ? firstCharCode - 32 : firstCharCode;
     return String.fromCharCode(firstCharCode) + this.slice(1);
-  }
+}
+
+/**
+ * Capitalizes the characters from the input string in the even and odd indexes
+ * @param {string} s 
+ * @returns {string[]}
+ */
+function capitalize(s){
+  return [s.replace(/\w/gi, (l, i) => i % 2 ? l.toLowerCase() : l.toUpperCase()), 
+          s.replace(/\w/gi, (l, i) => i % 2 ? l.toUpperCase() : l.toLowerCase())];
+};
