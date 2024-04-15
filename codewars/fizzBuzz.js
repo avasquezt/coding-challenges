4bd3bcc10f616538b7552400efce9ed17f4d4c3c
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-04-15 16:57:30 -0500

    Returns the fizzBuzz from the numbers from 1 to N

--- codewars/fizzBuzz.js
+++ codewars/fizzBuzz.js
@@ -8,3 +8,19 @@ function fizzBuzz(n){
 }
 
 console.log(fizzBuzz(15));
+
+/**
+ * Returns the fizzBuzz from the numbers from 1 to N
+ * @param {number} n 
+ * @returns {any[]}
+ */
+function fizzbuzz(n){
+    const ans = [];
+    for(let i = 1; i <= n; i++){
+      let text = '';
+      if(i % 3 == 0) text += 'Fizz';
+      if(i % 5 == 0) text += 'Buzz';
+      ans.push(text || i);
+    }
+    return ans;
+}
