69a5c3e6097985a8be7ff7ba0b20544848023f3e
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-05-16 15:44:38 -0500

    Returns an array with the properties and values of the input object

---
+++ codewars/convertHashToArray.js
@@ -0,0 +1,8 @@
+/**
+ * Returns an array with the properties and values of the input object
+ * @param {object} hash 
+ * @returns {string[]}
+ */
+function convertHashToArray(hash){
+    return  Object.entries(hash);
+}
