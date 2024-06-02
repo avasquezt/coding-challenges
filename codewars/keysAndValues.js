da5fc77155139182e50ccd4fc65a9ded4bb37a2d
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-06-02 16:43:46 -0500

    Returns the keys and values of the input object

---
+++ codewars/keysAndValues.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the keys and values of the input object
+ * @param {object[]} data 
+ * @returns {string[][]}
+ */
+function keysAndValues(data){
+    return [Object.keys(data), Object.values(data)];
+}
