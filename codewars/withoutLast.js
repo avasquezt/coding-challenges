6b1636ce081d9f2cfe18fb7b4b6d6f4eb386c8c9
avasquezt <alejandro.vasquez.taborda@gmail.com>
2024-06-19 15:16:04 -0500

    Removes the last element from the input array

---
+++ codewars/withoutLast.js
@@ -0,0 +1,9 @@
+/**
+ * Removes the last element from the input array
+ * @param {number[]} arr 
+ * @returns {number[]}
+ */
+function withoutLast(arr) {
+    // Fix it
+    return arr.slice(0,-1);
+}
